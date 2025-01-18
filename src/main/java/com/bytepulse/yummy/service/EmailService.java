a35e44820ea6fd02921f81377293c9daa8d5bb26
yadamoon <yared241993@gmail.com>
2025-01-18 18:53:47 +0300

    Created EmailService interface for email sending functionality

---
+++ src/main/java/com/bytepulse/yummy/service/EmailService.java
@@ -0,0 +1,15 @@
+package com.bytepulse.yummy.service;
+
+import com.bytepulse.yummy.model.EmailDetails;
+
+// Interface
+public interface EmailService {
+
+    // Method
+    // To send a simple email
+    String sendSimpleMail(EmailDetails details);
+
+    // Method
+    // To send an email with attachment
+    String sendMailWithAttachment(EmailDetails details);
+}
