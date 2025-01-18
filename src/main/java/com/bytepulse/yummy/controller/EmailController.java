570c7cf7c1b55a4622bb0ab633818a469aaf82bc
yadamoon <yared241993@gmail.com>
2025-01-18 18:50:26 +0300

    Created EmailController to handle email-related endpoints

---
+++ src/main/java/com/bytepulse/yummy/controller/EmailController.java
@@ -0,0 +1,38 @@
+package com.bytepulse.yummy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bytepulse.yummy.model.EmailDetails;
+import com.bytepulse.yummy.service.EmailService;
+
+// Annotation
+@RestController
+// Class
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    // Sending a simple Email
+    @PostMapping("/sendMail")
+    public String sendMail(@RequestBody EmailDetails details) {
+        System.out.println("inside >>>>>>>>>>>" + details);
+
+        String status = emailService.sendSimpleMail(details);
+        System.out.println("status >>>>>>>>>>>" + status);
+
+        return status;
+    }
+
+    // Sending email with attachment
+    @PostMapping("/sendMailWithAttachment")
+    public String sendMailWithAttachment(
+            @RequestBody EmailDetails details) {
+        String status = emailService.sendMailWithAttachment(details);
+
+        return status;
+    }
+}
