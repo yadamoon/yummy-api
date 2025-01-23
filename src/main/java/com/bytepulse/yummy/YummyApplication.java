09099f18e9e1dfb877bfb5c9929fff48a68029f7
yadamoon <yared241993@gmail.com>
2025-01-23 18:53:05 +0300

    chore: Configure Redis connection settings in YummyApplication

--- src/main/java/com/bytepulse/yummy/YummyApplication.java
+++ src/main/java/com/bytepulse/yummy/YummyApplication.java
@@ -2,10 +2,11 @@ package com.bytepulse.yummy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 // added swagger
-
+@EnableCaching
 public class YummyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YummyApplication.class, args);
